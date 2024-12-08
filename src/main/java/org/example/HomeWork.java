459ab3dd201185bd563435ee5ab590adbb2ad85b
ipiryutkina <irpir8@mail.ru>
2024-12-08 21:56:32 +0300

    реализован метод upit

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Scanner;
 
 public class HomeWork {
 
@@ -14,9 +15,43 @@ public class HomeWork {
      */
     @SneakyThrows
     public void upit(InputStream in, OutputStream out) {
+        Scanner scanner = new Scanner(in);
+        int n = scanner.nextInt();
+        int q = scanner.nextInt();
+        Treap treap = new Treap();
 
+        for (int i = 0; i < n; ++i) {
+            treap.add(scanner.nextInt());
+        }
+        for (int i = 0; i < q; ++i) {
+            int operation = scanner.nextInt();
+            int A, B, C, X;
+            switch (operation) {
+                case 1:
+                    A = scanner.nextInt() - 1;
+                    B = scanner.nextInt();
+                    X = scanner.nextInt();
+                    treap.case1(A, B, X);
+                    break;
+                case 2:
+                    A = scanner.nextInt() - 1;
+                    B = scanner.nextInt();
+                    X = scanner.nextInt();
+                    treap.case2(A, B, X);
+                    break;
+                case 3:
+                    C = scanner.nextInt() - 1;
+                    X = scanner.nextInt();
+                    treap.add(C, X);
+                    break;
+                case 4:
+                    A = scanner.nextInt() - 1;
+                    B = scanner.nextInt();
+                    Treap.Node node = treap.getStats(A, B);
+                    long sum = node.statistic.sumValue;
+                    out.write(String.valueOf(sum).getBytes());
+                    out.write("\r\n".getBytes());
+            }
+        }
     }
-
-
-
 }
